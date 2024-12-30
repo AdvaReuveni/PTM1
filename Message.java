2829b67452a55f48e7aa39307974dee376a49a35
AdvaReuveni <advushki@gmail.com>
2024-12-30 18:30:03 +0200

    ms 1

--- Message.java
+++ Message.java
@@ -1,9 +1,18 @@
 package test;
+import java.util.Date;
 
 import java.util.Date;
 
 public class Message {
     public final byte[] data;
+
+    public Message(byte[] data, String asText, double asDouble, Date date) {
+        this.data = data;
+        this.asText = asText;
+        this.asDouble = asDouble;
+        this.date = date;
+    }
+
     public final String asText;
     public final double asDouble;
     public final Date date;
--- Topic.java
+++ Topic.java
@@ -1,26 +1,39 @@
-package project_biu.graph;
+package test;
+import test.Agent;//איזה חבילות אני רוצה להשתמש
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Topic {
     public final String name;
+    private List<Agent> pubs;
+    private List<Agent> subs;
+
     Topic(String name){
+
         this.name=name;
+        this.pubs = new ArrayList<>();//לא חייב לרשום אגנט בסוגריים זה ידוע שזה זה
+        this.subs = new ArrayList<>();
     }
 
     public void subscribe(Agent a){
+        subs.add(a);
     }
     public void unsubscribe(Agent a){
+        subs.remove(a);
     }
 
-    public void publish(Message m){
+    public void publish(Message m) {//הדרכון מוכן
+        subs.parallelStream().forEach(a -> a.callback(name, m));//עובר על כל מי שבהאזנה
+
     }
 
     public void addPublisher(Agent a){
+        pubs.add(a);
     }
 
     public void removePublisher(Agent a){
+        pubs.remove(a);
     }
 
 
