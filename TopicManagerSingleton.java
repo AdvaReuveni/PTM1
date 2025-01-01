a15110471c614c8aece35ceed327de5ff96f72e9
AdvaReuveni <advushki@gmail.com>
2025-01-01 10:03:30 +0200

    created TopicManagerSingleton.java

--- TopicManagerSingleton.java
+++ TopicManagerSingleton.java
@@ -1,10 +1,44 @@
 package test;
 
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TopicManagerSingleton {
 
     public static class TopicManager{
+
+        Map<String,Topic> topics;
+        private static final TopicManager instance = new TopicManager();
+        private TopicManager()
+        {
+            topics = new HashMap<String,Topic>();
+        }
+
+        public Topic getTopic(String nameOfTopic)
+        {
+            if(!topics.containsKey(nameOfTopic))
+            {
+                topics.put(nameOfTopic, new Topic(nameOfTopic));
+            }
+            return topics.get(nameOfTopic);
+        }
+        public Collection<Topic> getTopics()//return collection of Topics. The values without the keys
+        {
+            return topics.values();
+        }
+
+        public void clear()
+        {
+            topics.clear();
+        }
     }
 
+    public static TopicManager get()
+    {
+        return TopicManager.instance;
+    }
     
 }
+
